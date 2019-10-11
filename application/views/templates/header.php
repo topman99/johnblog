ba303401e072042c6c0fce2d1bb118328dd4e1e1
topman99 <topman99@gmail.com>
2019-10-11 14:53:06 +0800

    First Commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ciBlog</title>
+	<link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+</head>
+	<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+	  	  <div class="navbar-header">
+			<a class="navbar-brand" href="">ciBlog</a>
+		  </div>
+
+		  <div id="navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		  </div>
+		</div>
+	</nav>
+
+		    
